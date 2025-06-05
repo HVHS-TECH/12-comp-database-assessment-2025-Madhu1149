9a02052e384ae706be4730a4188a05d0f4263b2d
Madhu1149 <23477mt@hvhs.school.nz>
2025-06-05 22:35:39 +0000

    making the layout and initialising

---
+++ main.mjs
@@ -0,0 +1,19 @@
+/**************************************************************/
+// main.mjs
+// Main entry for index.html
+// Written by <Madhulika Tirindi>, Term 2 2025
+/**************************************************************/
+/**************************************************************/
+// Import all external constants & functions required
+/**************************************************************/
+// Import all the constants & functions required from fb_io module
+import { fb_initialise }
+    from './fb_io.mjs';
+    window.fb_initialise   = fb_initialise;
+    
+import { fb_authenticate }
+    from './fb_io.mjs';
+    window.fb_authenticate   = fb_authenticate;
+/**************************************************************/
+//   END OF CODE
+/**************************************************************/
